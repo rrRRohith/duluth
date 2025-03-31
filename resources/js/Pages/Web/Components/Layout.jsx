080c17cc8be60bbc4e3320b4f9b42bda572b2b7a
Rohith R <88495305+rrRRohith@users.noreply.github.com>
2025-03-31 20:27:36 +0530

    flash message

--- resources/js/Pages/Web/Components/Layout.jsx
+++ resources/js/Pages/Web/Components/Layout.jsx
@@ -1,8 +1,10 @@
 import { Head } from "@inertiajs/react";
+import Flash from "@/Components/Flash";
 
 export default function Layout({ title, children }) {
     return (
         <>
+            <Flash />
             <Head title={title}></Head>
             {children}
         </>
